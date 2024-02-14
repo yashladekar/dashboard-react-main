5bbca07d2d8d68e5aae418522f1448d0e4416e79
yashladekar <yashladekar002@gmail.com>
2024-02-14 20:16:47 +0530

    remove setting button and changed footer

--- src/widgets/layout/footer.jsx
+++ src/widgets/layout/footer.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { Typography } from "@material-tailwind/react";
-import { HeartIcon } from "@heroicons/react/24/solid";
+
 
 export function Footer({ brandName, brandLink, routes }) {
   const year = new Date().getFullYear();
@@ -9,14 +9,14 @@ export function Footer({ brandName, brandLink, routes }) {
     <footer className="py-2">
       <div className="flex w-full flex-wrap items-center justify-center gap-6 px-2 md:justify-between">
         <Typography variant="small" className="font-normal text-inherit">
-          &copy; {year}, made with{" "}
-          <HeartIcon className="-mt-0.5 inline-block h-3.5 w-3.5 text-red-600" /> by{" "}
+          &copy; {year}, made {" "}
+          by{" "}
           <a
-            href={brandLink}
-            target="_blank"
+            href='#'
+
             className="transition-colors hover:text-blue-500 font-bold"
           >
-            {brandName}
+            Group-16
           </a>{" "}
           for a better web.
         </Typography>
@@ -44,10 +44,10 @@ Footer.defaultProps = {
   brandName: "Creative Tim",
   brandLink: "https://www.creative-tim.com",
   routes: [
-    { name: "Creative Tim", path: "https://www.creative-tim.com" },
-    { name: "About Us", path: "https://www.creative-tim.com/presentation" },
-    { name: "Blog", path: "https://www.creative-tim.com/blog" },
-    { name: "License", path: "https://www.creative-tim.com/license" },
+    { name: "Yash Ladekar", path: "https://github.com/yashladekar" },
+    { name: "Vaibhav Bhatkare", path: "https://github.com/vaibhavbhatkare" },
+    { name: "Rahul Mahajan", path: "https://github.com/rahulmahajan000" },
+    { name: "Abhiest Yadav", path: "https://github.com/" },
   ],
 };
 
